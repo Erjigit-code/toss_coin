508d5234537c38c1a4bccfd4b4ed06f086326353
Erjigit Abdykaarov <72736628+Erjigit-code@users.noreply.github.com>
2024-07-02 23:55:19 +0600

    added some style to settings

--- lib/screens/settings_screen/settings_screen.dart
+++ lib/screens/settings_screen/settings_screen.dart
@@ -1,3 +1,5 @@
+import 'dart:ui';
+
 import 'package:coin_flip/screens/statistics_screen/statistics_screen.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter_bloc/flutter_bloc.dart';
@@ -27,35 +29,78 @@ class SettingsScreenState extends State<SettingsScreen> {
     print("SettingsScreen initState: selectedCoin=$selectedCoin");
   }
 
+  Widget _buildWrappedContainer(Widget child) {
+    return Container(
+      height: 60,
+      width: MediaQuery.of(context).size.width * 0.9,
+      margin: EdgeInsets.symmetric(vertical: 10),
+      decoration: BoxDecoration(
+        color: Colors.white.withOpacity(0.3),
+        borderRadius: BorderRadius.circular(45),
+      ),
+      child: Center(child: child),
+    );
+  }
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
+      extendBodyBehindAppBar: true,
       appBar: AppBar(
+        backgroundColor: Colors.transparent,
         title: const Text("Настройки"),
       ),
-      body: Column(
-        crossAxisAlignment: CrossAxisAlignment.start,
+      body: Stack(
         children: [
-          BackgroundOption(selectedImage: selectedImage),
-          CoinOption(selectedCoin: selectedCoin),
-          ListTile(
-            title: const Text("Статистика"),
-            onTap: () {
-              print("Statistics button tapped");
-              BlocProvider.of<CoinBloc>(context).add(LoadStatistics());
-              Navigator.push(
-                context,
-                MaterialPageRoute(
-                  builder: (context) => StatisticsScreen(),
-                ),
-              ).then((value) {
-                BlocProvider.of<CoinBloc>(context).add(LoadCoinPreferences());
-                print("Returning to MainScreen, reloading preferences...");
-              });
-            },
+          Container(
+            decoration: const BoxDecoration(
+              image: DecorationImage(
+                image: AssetImage('assets/background/back.jpeg'),
+                fit: BoxFit.cover,
+              ),
+            ),
           ),
-          const Divider(),
-          ResetRecordButton(),
+          BackdropFilter(
+            filter: ImageFilter.blur(sigmaX: 5.0, sigmaY: 5.0),
+            child: Container(
+              color: Colors.white.withOpacity(0.1),
+            ),
+          ),
+          Padding(
+            padding: const EdgeInsets.only(top: kToolbarHeight + 16.0),
+            child: Center(
+              child: Column(
+                children: [
+                  _buildWrappedContainer(
+                      BackgroundOption(selectedImage: selectedImage)),
+                  _buildWrappedContainer(
+                      CoinOption(selectedCoin: selectedCoin)),
+                  _buildWrappedContainer(
+                    ListTile(
+                      title: const Text("Статистика"),
+                      onTap: () {
+                        print("Statistics button tapped");
+                        BlocProvider.of<CoinBloc>(context)
+                            .add(LoadStatistics());
+                        Navigator.push(
+                          context,
+                          MaterialPageRoute(
+                            builder: (context) => StatisticsScreen(),
+                          ),
+                        ).then((value) {
+                          BlocProvider.of<CoinBloc>(context)
+                              .add(LoadCoinPreferences());
+                          print(
+                              "Returning to MainScreen, reloading preferences...");
+                        });
+                      },
+                    ),
+                  ),
+                  ResetRecordButton(),
+                ],
+              ),
+            ),
+          ),
         ],
       ),
     );
